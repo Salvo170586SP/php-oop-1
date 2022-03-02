8d200bb9336b77115aaa20da8622f29dd0e34b83
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-02 15:56:56 +0100

    added dates inner card

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 */
 
 $movies = [
-    ['id'=> '5185', 'title'=>'Il Gladiatore','genere'=>'storico','years'=>'2000'],
-    ['id'=> '3443', 'title'=>'Il Padrino','genere'=>'drammatico ','years'=>'1972'],
-    ['id'=> '4543', 'title'=>'Ritorno al futuro','genere'=>'fantascienza','years'=>'1985'],
-    ['id'=> '2232', 'title'=>'Spider-Man: No Way Home','genere'=>'avventura','years'=>'2021'],
-    ['id'=> '6743', 'title'=>'Salvate il soldato Ryan','genere'=>'storico','years'=>'1998']
+    ['id'=> '5185', 'title'=>'Il Gladiatore','genre'=>'storico','years'=>'2000'],
+    ['id'=> '3443', 'title'=>'Il Padrino','genre'=>'drammatico ','years'=>'1972'],
+    ['id'=> '4543', 'title'=>'Ritorno al futuro','genre'=>'fantascienza','years'=>'1985'],
+    ['id'=> '2232', 'title'=>'Spider-Man: No Way Home','genre'=>'avventura','years'=>'2021'],
+    ['id'=> '6743', 'title'=>'Salvate il soldato Ryan','genre'=>'storico','years'=>'1998']
 ];
 
 class Movie
@@ -49,12 +49,14 @@ class Movie
         <?php foreach($movies as $movie):
             $obj_movie = new Movie($movie['id'], $movie['title'], $movie['genre'], $movie['years']) 
         ?>
+
         <!-- card-movie -->
         <div class="card-movie">
-           
+           <h2><?= $obj_movie->title ?></h2>
+           <p><?= $obj_movie->genre ?></p>
+           <span><?= $obj_movie->years ?></span>
         </div>
-        
-        
+           
         <?php endforeach?>
     </div>
 </body>
