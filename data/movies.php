9e3557da6d8b2176f9f549d1a0f78e6e5eedc258
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-02 16:28:16 +0100

    added   data api

---
+++ data/movies.php
@@ -0,0 +1,9 @@
+<?php
+
+$movies = [
+    ['id'=> '5185', 'title'=>'Il Gladiatore','genre'=>'storico','years'=>'2000'],
+    ['id'=> '3443', 'title'=>'Il Padrino','genre'=>'drammatico ','years'=>'1972'],
+    ['id'=> '4543', 'title'=>'Ritorno al futuro','genre'=>'fantascienza','years'=>'1985'],
+    ['id'=> '2232', 'title'=>'Spider-Man: No Way Home','genre'=>'avventura','years'=>'2021'],
+    ['id'=> '6743', 'title'=>'Salvate il soldato Ryan','genre'=>'storico','years'=>'1998']
+];
--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-$movies = [
-    ['id'=> '5185', 'title'=>'Il Gladiatore','genre'=>'storico','years'=>'2000'],
-    ['id'=> '3443', 'title'=>'Il Padrino','genre'=>'drammatico ','years'=>'1972'],
-    ['id'=> '4543', 'title'=>'Ritorno al futuro','genre'=>'fantascienza','years'=>'1985'],
-    ['id'=> '2232', 'title'=>'Spider-Man: No Way Home','genre'=>'avventura','years'=>'2021'],
-    ['id'=> '6743', 'title'=>'Salvate il soldato Ryan','genre'=>'storico','years'=>'1998']
-];
+require __DIR__ . '/data/movies.php';
 
 class Movie
 {
